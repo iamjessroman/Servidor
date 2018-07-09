f785564943ff6dbd2a34466054c6bbc402218411
jessicaroman <jessicaromanlargo@gmail.com>
2018-07-08 19:52:43 -0500

    first commit

---
+++ src/java/Download.java
@@ -0,0 +1,44 @@
+
+import java.io.File;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author jessi
+ */
+@Path("/descarga")
+public class Download {
+    
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_PLAIN })
+    @Path("/json")
+    public Response downloadJson() {
+        File file = new File("C:\\Parqueaderos\\1.json");
+        ResponseBuilder response = Response.ok((Object) file);
+        response.header("Content-Disposition", "attachment;filename=Json.json");
+        return response.build();
+    }
+    
+        
+    @GET
+    @Produces("image/jpg")
+    @Path("/image")
+    public Response downloadImage() {
+        File file = new File("C:\\Parqueaderos\\1.jpg");
+        ResponseBuilder response = Response.ok((Object) file);
+        response.header("Content-Disposition", "attachment;filename=Image.jpg");
+        return response.build();
+    }
+
+}
